cac3b064ebbdf03d1aba8bc7e7e6bc2b387f199b
Brahim Saadaoui <117919053+BrahimSD@users.noreply.github.com>
2024-10-01 12:27:40 +0200

    brahim_guillaume

---
+++ src/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Tache : " + title + " - " + description + " | Statut : " + (completed ? "Terminee" : "Non terminee");
+    }
+}
